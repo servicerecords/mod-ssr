ab6ca115a37d3bb4e76b1474e817c0be9cf7251a
Simon Ainley <30348475+87Development@users.noreply.github.com>
2020-01-27 14:39:27 +0000

    removed lines of text that are no longer needed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,15 +7,6 @@
     <p class="govuk-body-l">Use this service to obtain information relating to a serviceman/servicewoman who has served
     in the Royal Navy, Royal Marines, Army, Royal Air Force or Home Guard.</p>
 
-    <h2 class="govuk-heading-m">Before you start</h2>
-
-    <ul class="govuk-list govuk-list--bullet">
-        <li>A death certificate may be required in some cases (<a class="govuk-link" href="https://www.gov.uk/guidance/request-records-of-deceased-service-personnel">see
-                notes</a>)</li>
-        <li>An administrative fee may apply (<a class="govuk-link" href="https://www.gov.uk/guidance/request-records-of-deceased-service-personnel#administration-fee">see
-                notes</a>)</li>
-    </ul>
-
     <a href="/service" role="button" draggable="false" class="govuk-button govuk-button--start">
         Start now
     </a>
