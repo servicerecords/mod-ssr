56ce5c05a47b9a73689eb0cec744a680799ef23c
Simon Ainley <30348475+87Development@users.noreply.github.com>
2020-02-27 21:23:15 +0000

    fix payment link in confirmation view

--- resources/views/confirmation.blade.php
+++ resources/views/confirmation.blade.php
@@ -18,6 +18,6 @@
     </p>
 
     <p class="govuk-body">
-        <a href="/feeback" class="govuk-link">What did you think of this service?</a> (takes 30 seconds)
+        <a href="/feedback" class="govuk-link">What did you think of this service?</a> (takes 30 seconds)
     </p>
 @endsection
