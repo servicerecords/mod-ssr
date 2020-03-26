bd95c971ed3fa26aba1891a959112132e47b69f1
Simon Ainley <30348475+87Development@users.noreply.github.com>
2020-03-26 23:29:09 +0000

    image compression

--- app/Http/Controllers/ServiceRecordController.php
+++ app/Http/Controllers/ServiceRecordController.php
@@ -354,7 +354,7 @@ class ServiceRecordController extends Controller
         //$validation = $request->validated();
 
         if(strpos($request->file('certificate')->getMimeType(), "image") !== false) {
-            Storage::makeDirectory('verfication');
+            Storage::makeDirectory('verification');
             $resized_file = storage_path('app/verification/') . $request->session()->get('reference') . '-resized.jpg';
             /*
             $original_file = Storage::disk('local')->put('verification', $request->file('certificate'));
