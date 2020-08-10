cce0872ab482ac360fec30d7a5e7bb70ba66f98b
Toby Liddicoat <toby@codesure.co.uk>
2020-08-10 06:35:44 +0100

    Dusk Selenium testing library loaded. This will ship with production but not too much we can do about that at this point.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,6 +41,8 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+$app->register(Laravel\Dusk\DuskServiceProvider::class);
+
 /*
 |--------------------------------------------------------------------------
 | Return The Application
