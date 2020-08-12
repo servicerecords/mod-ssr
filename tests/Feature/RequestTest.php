328f9ff2ccac8de5bbbca5483cb63f71bef5fdef
Toby Liddicoat <toby@codesure.co.uk>
2020-08-12 05:17:00 +0100

    All files conform to PSR standards

--- tests/Feature/RequestTest.php
+++ tests/Feature/RequestTest.php
@@ -3,9 +3,6 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class RequestTest extends TestCase
 {
@@ -13,7 +10,7 @@ class RequestTest extends TestCase
      * A basic feature test example.
      *
      * @return void
-	 * @test
+     * @test
      */
     public function a_user_can_start_a_request()
     {
@@ -24,16 +21,16 @@ class RequestTest extends TestCase
     }
 
     /**
-	 * @test
-	 */
+     * @test
+     */
     public function a_user_save_their_request_choice()
-	{
-		$request = [
-			'reqtype' => 'Deceased',
-		];
+    {
+        $request = [
+            'reqtype' => 'Deceased',
+        ];
 
-		$response = $this->post('/request', $request);
-		$response->assertStatus(302);
-		$response->assertRedirect('/service');
-	}
+        $response = $this->post('/request', $request);
+        $response->assertStatus(302);
+        $response->assertRedirect('/service');
+    }
 }
