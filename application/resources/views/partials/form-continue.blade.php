5ef3faae987120252bca2fadd3fe94b54b77e26f
Toby Liddicoat <toby@codesure.co.uk>
2020-10-19 14:58:16 +0100

    Set secondary button as Leave Now

--- application/resources/views/partials/form-continue.blade.php
+++ application/resources/views/partials/form-continue.blade.php
@@ -1,5 +1,5 @@
 <div class="govuk-form-group govuk-!-margin-top-5">
     @csrf
     <button type="submit" class="govuk-button">Continue</button>
-    <a class="govuk-button govuk-!-margin-left-9" href="/leave">Leave Now</a>
+    <a class="govuk-button govuk-button--secondary" href="/leave">Leave Now</a>
 </div>
