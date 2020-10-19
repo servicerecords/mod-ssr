91e6918577ffdb8090a0c4f612a198cf4f24a0ec
Toby Liddicoat <toby@codesure.co.uk>
2020-10-19 15:23:50 +0100

    Bumped button with right margin

--- application/resources/views/partials/form-continue.blade.php
+++ application/resources/views/partials/form-continue.blade.php
@@ -1,5 +1,5 @@
 <div class="govuk-form-group govuk-!-margin-top-5">
     @csrf
-    <button type="submit" class="govuk-button">Continue</button>
+    <button type="submit" class="govuk-button govuk-!-margin-right-1">Continue</button>
     <a class="govuk-button govuk-button--secondary" href="/leave">Leave Now</a>
 </div>
