f1ca65cafa886d44c047e49ee72e1ae5c2eb6106
Toby Liddicoat <toby@codesure.co.uk>
2020-11-09 06:02:23 +0000

    Fixed feedback rendering

---
+++ src/resources/views/feedback-complete.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('pageTitle', 'Give feedback')
+
+@section('content')
+    <div class="govuk-panel govuk-panel--confirmation">
+        <h1 class="govuk-panel__title">
+            Feedback submission complete
+        </h1>
+        <div class="govuk-panel__body">
+            Thank you for your feedback on the {{ env('APP_NAME', 'Apply for a deceased persons military record') }} service
+        </div>
+    </div>
+@endsection
