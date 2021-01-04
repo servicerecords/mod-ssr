9fadd60819486af67484ae90f21fb4e3d2cbc9f9
Toby Liddicoat <toby@codesure.co.uk>
2021-01-04 11:44:45 +0000

    Testfield type can be over-ridden

--- src/app/View/Components/Textfield.php
+++ src/app/View/Components/Textfield.php
@@ -17,15 +17,21 @@ class Textfield extends FormField
      */
     public $maxlength = 120;
 
+    /**
+     * @var string
+     */
+    public $type = 'text';
+
     public function __construct($field = null, $label = 'Option', $value = null, $hint = false, $selected = null,
                                 $options = [], $labelExtra = null, $mandatory = true, $characterLimit = false,
                                 $fullWidth = false, $autocomplete = false, $hideLabel = false, $spellcheck = true,
-                                $maxlength = 120)
+                                $maxlength = 120, $type= 'text')
     {
         parent::__construct($field, $label, $value, $hint, $selected, $options, $labelExtra, $mandatory, $characterLimit, $fullWidth, $autocomplete, $hideLabel);
 
         $this->spellcheck = $spellcheck;
         $this->maxlength = $maxlength;
+        $this->type = $type;
     }
 
     /**
