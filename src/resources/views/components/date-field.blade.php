f017995155ac2568120f2fb5332a1fc07a458a7e
Toby Liddicoat <toby@codesure.co.uk>
2021-01-11 15:25:34 +0000

    Changed GDS component to a Fieldset with Legend

--- src/resources/views/components/date-field.blade.php
+++ src/resources/views/components/date-field.blade.php
@@ -1,29 +1,38 @@
 <div
     class="govuk-form-group @if($errors->get($field .'-day') || $errors->get($field .'-month') || $errors->get($field .'-year')) govuk-form-group--error @endif"
     aria-describedby="@if($hint){{ $field }}-hint @endif @error($field) {{ $field }}-year-error @enderror">
-    <x-label :field="$field" :label="$label" :extra="$labelExtra" :mandatory="$mandatory" :hidden="false"></x-label>
-    <x-hint :hint="$hint" :field="$field"></x-hint>
-    <x-error-message :field="$field"></x-error-message>
-    <x-error-message :field="$field .'-day'"></x-error-message>
-    <x-error-message :field="$field .'-month'"></x-error-message>
-    <x-error-message :field="$field .'-year'"></x-error-message>
 
+    <fieldset class="govuk-fieldset">
+        <legend class="govuk-fieldset__legend govuk-fieldset__legend--l">
+            <h1 class="govuk-fieldset__heading">
+                {{ $label }}
+            </h1>
+        </legend>
 
-    <div class="govuk-date-input" id="{{ $field }}">
-        @if($singleField)
-            <x-date-input :hideLabel="true" :field="$field" :period="$period"></x-date-input>
-        @else
-            @if(!$hideDay)
-                <x-date-input :hideLabel="$hideDayLabel" :label='$dayLabel' :field="$field" period="day"></x-date-input>
+        {{--    <x-label :field="$field" :label="$label" :extra="$labelExtra" :mandatory="$mandatory" :hidden="false"></x-label>--}}
+        <x-hint :hint="$hint" :field="$field"></x-hint>
+        <x-error-message :field="$field"></x-error-message>
+        <x-error-message :field="$field .'-day'"></x-error-message>
+        <x-error-message :field="$field .'-month'"></x-error-message>
+        <x-error-message :field="$field .'-year'"></x-error-message>
+
+        <div class="govuk-date-input" id="{{ $field }}">
+            @if($singleField)
+                <x-date-input :hideLabel="true" :field="$field" :period="$period"></x-date-input>
+            @else
+                @if(!$hideDay)
+                    <x-date-input :hideLabel="$hideDayLabel" :label='$dayLabel' :field="$field"
+                                  period="day"></x-date-input>
+                @endif
+                @if(!$hideMonth)
+                    <x-date-input :hideLabel="$hideMonthLabel" :label='$monthLabel' :field="$field"
+                                  period="month"></x-date-input>
+                @endif
+                @if(!$hideYear)
+                    <x-date-input :hideLabel="$hideYearLabel" :label='$yearLabel' :field="$field"
+                                  period="year"></x-date-input>
+                @endif
             @endif
-            @if(!$hideMonth)
-                <x-date-input :hideLabel="$hideMonthLabel" :label='$monthLabel' :field="$field"
-                              period="month"></x-date-input>
-            @endif
-            @if(!$hideYear)
-                <x-date-input :hideLabel="$hideYearLabel" :label='$yearLabel' :field="$field"
-                              period="year"></x-date-input>
-            @endif
-        @endif
-    </div>
+        </div>
+    </fieldset>
 </div>
--- src/resources/views/components/date-input.blade.php
+++ src/resources/views/components/date-input.blade.php
@@ -1,5 +1,5 @@
 @props([
-    'overLabel' => '',
+    'accessibleLabel' => '',
     'label'     => '',
     'field'     => '',
     'period'    => '',
@@ -10,14 +10,14 @@
     <div class="govuk-form-group">
         @if(!$hideLabel)
             <label class="govuk-label govuk-date-input__label" for="{{ $field }}-{{ $period }}">
-                <span ></span>
+                @if($accessibleLabel)<span class="govuk-visually-hidden">{{ $accessibleLabel }}</span>@endif
                 {{ $label }}
             </label>
-        @endif
-        <input
+        @endif<input
             class="govuk-input govuk-date-input__input govuk-input--width-2 @error($field .'-' . $period) govuk-input--error @enderror"
             id="{{ !$hideLabel ? $field . '-' . $period : $field }}"
             name="{{ $field }}-{{ $period }}" type="text" pattern="[0-9]*" inputmode="numeric"
             value="{{ old($field . '-' . $period, session($field. '-' . $period)) }}">
+
     </div>
 </div>
