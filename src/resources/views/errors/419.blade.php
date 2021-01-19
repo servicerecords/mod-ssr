19eafa6606203d7c1e10bcdd8712cc877b374fc0
Toby Liddicoat <toby@codesure.co.uk>
2021-01-19 15:44:17 +0000

    new 419 content

--- src/resources/views/errors/419.blade.php
+++ src/resources/views/errors/419.blade.php
@@ -3,3 +3,17 @@
 @section('pageTitle', __('Page Expired'))
 @section('code', '419')
 @section('message', __('Page Expired'))
+
+
+@section('content')
+    <p class="govuk-body">
+        If you typed the web address, check it is correct.
+    </p>
+    <p class="govuk-body">
+        If you pasted the web address, check you copied the entire address.
+    </p>
+    <p class="govuk-body">
+        If the web address is correct or you selected a link or button,
+        You will need to <a href="/service">Restart your application</a>.
+    </p>
+@endsection
